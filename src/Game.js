41df7eae679f0b243184fdf5cb1f57d280ea3f03
Sarah <sarahrudy@gmail.com>
2021-07-08 15:08:04 -0600

    Add pseudocode for Game class

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,24 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Round = require('./Round')
 
 class Game {
   constructor() {
     this.currentRound = currentRound
+    // should keep track of current round
+  }
+
+  start() {
+    // method that starts everything
+    // creates Cards (new Card)
+    // create new Round (new Round) using Deck( new Deck)
+  
+    // printMessage()
+    // printQuestion()
+
   }
 
   printMessage(deck, round) {
