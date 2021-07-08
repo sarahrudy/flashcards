7c7ea2826df5a00dc2ff4bd51ed888c128cf6cb9
Sarah <sarahrudy@gmail.com>
2021-07-08 17:52:00 -0600

    Write tests for Game class

--- test/Game-test.js
+++ test/Game-test.js
@@ -13,11 +13,19 @@ describe('Game', function() {
     game = new Game()
   })
 
+  it('should be a function', function() {
+    expect(Game).to.be.a('function')
+  })
+
   it('should be an instance', function() {
     expect(game).to.be.an.instanceOf(Game)
   })
 
   it('should be able to track current round', function() {
     expect(game.currentRound).to.equal(null)
+  })
+
+  it('should be able to start game', function() {
+    expect(game.start).to.be.a('function')
   })
 })
